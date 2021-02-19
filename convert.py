8ce2aa2fc3e3252b9e8df733d9f2e61740d60a08
fridos <fridmahdi@gmail.com>
2021-02-19 09:14:57 +0100

    chnage to list of dict

--- convert.py
+++ convert.py
@@ -1,35 +1,40 @@
 import json
+import pandas as pd
+from itertools import zip_longest
 
-listsUnits = {}
-completelist = []
+completelist, finalelist = [] , []
 itemValue = 'pricePerUnit'
 listKeys = ['instanceType', 'memory', 'vcpu', 'storage', 'networkPerformance', 'tenancy']
 
 with open('/Users/mahdifrid/Desktop/PricingCloud/file.json', 'r') as data : 
     json_data = json.load(data)
 
-instanceType = (json_data["product"]["attributes"])
-for key, value in instanceType.items():
-    for keyInstance in listKeys :
-        if key in keyInstance:
-            completelist.append("{}: {}".format(key, value)) 
+# function get instance type
+for i in range(len(json_data)):
+    listsUnits = {}
+    instanceType = (json_data[i]["product"]["attributes"])
+    for key, value in instanceType.items():
+        for keyInstance in listKeys :
+            if key in keyInstance:
+                listsUnits.update({key:value}) 
+    completelist.append(listsUnits)
 
 
-termsInstance = (json_data["terms"]["OnDemand"])
-pricingItems = [dict_pricing['priceDimensions'] for dict_pricing in termsInstance.values()]
+# function get pricing list            
+for i in range(len(json_data)):
+    listUnitsmatch = {}
+    listPricesinstance = {}
+    termsInstance = (json_data[i]["terms"]["OnDemand"])
+    pricingItems = [dict_pricing['priceDimensions'] for dict_pricing in termsInstance.values()]  
+    princingUnit = json.dumps(pricingItems)
+    unitDict = json.loads(princingUnit)
+    for element in unitDict:
+        for key, value in element.items():
+            listPricesinstance = value
+    for key, value in listPricesinstance.items():
+        if key in itemValue:
+            listUnitsmatch.update(value)
+    finalelist.append(listUnitsmatch)
 
-princingUnit = json.dumps(pricingItems)
-unitDict = json.loads(princingUnit)
-for element in unitDict:
-    for key, value in element.items():
-        listsUnits = value
-
-for key, value in listsUnits.items():
-    if key in itemValue:
-        completelist.append("{}: {}".format(key, value)) 
-
-print(completelist)
-print(len(completelist))
-
-#print((json_data["product"]["attributes"]["instanceType"])+": "+(json_data["product"]["attributes"]["memory"])+ ".RAM")
-
+listec2Instance = [{**u, **v} for u, v in zip_longest(completelist, finalelist, fillvalue={})]
+print(json.dumps(listec2Instance, indent=4))
